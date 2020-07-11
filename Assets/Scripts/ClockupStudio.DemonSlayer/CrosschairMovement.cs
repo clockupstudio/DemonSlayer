fcc700e6594e9a2617f8bdae043a6e79dfcfc104
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2020-07-11 14:48:15 +0700

    Fix bug incorrect move player to crosschair pointer
    Include set gravity before and after move over direction.

---
+++ Assets/Scripts/ClockupStudio.DemonSlayer/CrosschairMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace ClockupStudio.DemonSlayer
+{
+    public class CrosschairMovement : MonoBehaviour
+    {
+        public Transform PlayerPosition;
+
+        public float Radius = 2f;
+
+        public float Speed = 2f;
+
+        private float _angle = 0f;
+
+        // Update is called once per frame
+        private void Update()
+        {
+            var position = PlayerPosition.position;
+            var x = Mathf.Cos(Mathf.PI * 2 * _angle / 360) * Radius;
+            var y = Mathf.Sin(Mathf.PI * 2 * _angle / 360) * Radius;
+
+            Debug.Log($"New Position: {x}, {y}");
+            transform.position = new Vector3(x, y) + position;
+            _angle += Time.deltaTime * Speed;
+            Debug.Log($"Current angle: {_angle}");
+            if (_angle >= 90f)
+            {
+                _angle = 0f;
+            }
+        }
+    }
+}
--- Assets/Scripts/ClockupStudio.DemonSlayer/PlayerMovement.cs
+++ Assets/Scripts/ClockupStudio.DemonSlayer/PlayerMovement.cs
@@ -6,24 +6,39 @@ namespace ClockupStudio.DemonSlayer
     public class PlayerMovement : MonoBehaviour
     {
         public float Force;
+        public float Distance = 2f;
 
         private Rigidbody2D _rb2d;
+        private Vector3 _origin;
 
         // Start is called before the first frame update
         private void Start()
         {
             _rb2d = GetComponent<Rigidbody2D>();
+            _origin = transform.position;
         }
 
         // Update is called once per frame
         private void Update()
         {
+            if (_rb2d.velocity == Vector2.zero)
+            {
+                return;
+            }
+
+            // Debug.Log($"Origin: {_origin}");
+            if (Vector2.Distance(_origin, transform.position) >= Distance)
+            {
+                _rb2d.velocity = Vector2.zero;
+                _rb2d.gravityScale = 1;
+            }
         }
 
-        public void Move(Vector2 crosscharPosition)
+        public void Move(Vector2 crosschairPosition)
         {
-            // TODO(wingyplus): ignore gravity.
-            _rb2d.velocity = crosscharPosition.normalized * new Vector2(Force, Force);
+            _origin = transform.position;
+            _rb2d.gravityScale = 0;
+            _rb2d.velocity = (crosschairPosition - (Vector2) _origin).normalized * new Vector2(Force, Force);
         }
     }
 }
