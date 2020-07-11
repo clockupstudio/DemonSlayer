ca7f1dd82646be3b03575170c4e93dc9d303d7e3
Prayoch Rujira <prayoch@clockupstudio.com>
2020-07-11 15:14:47 +0700

    Crosschair can move in counter clockwise

--- Assets/Scripts/ClockupStudio.DemonSlayer/CrosschairMovement.cs
+++ Assets/Scripts/ClockupStudio.DemonSlayer/CrosschairMovement.cs
@@ -2,6 +2,11 @@
 
 namespace ClockupStudio.DemonSlayer
 {
+    enum Clockwises
+    {
+        Clockwise = 1,
+        CounterClockwise = -1
+    }
     public class CrosschairMovement : MonoBehaviour
     {
         public Transform PlayerPosition;
@@ -12,20 +17,24 @@ namespace ClockupStudio.DemonSlayer
 
         private float _angle = 0f;
 
+        private Clockwises _clockWise = Clockwises.Clockwise;
+
         // Update is called once per frame
         private void Update()
         {
             var position = PlayerPosition.position;
             var x = Mathf.Cos(Mathf.PI * 2 * _angle / 360) * Radius;
             var y = Mathf.Sin(Mathf.PI * 2 * _angle / 360) * Radius;
-
-            Debug.Log($"New Position: {x}, {y}");
+            
             transform.position = new Vector3(x, y) + position;
-            _angle += Time.deltaTime * Speed;
-            Debug.Log($"Current angle: {_angle}");
-            if (_angle >= 90f)
+            _angle += (Time.deltaTime * Speed) * (int)_clockWise;
+            if (_clockWise == Clockwises.Clockwise && _angle >= 90f)
             {
-                _angle = 0f;
+                _clockWise = Clockwises.CounterClockwise;
+            }
+            else if (_clockWise == Clockwises.CounterClockwise && _angle <= 0)
+            {
+                _clockWise = Clockwises.Clockwise;
             }
         }
     }
