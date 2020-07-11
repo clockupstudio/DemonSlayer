571dcea0348c309ec5f898b84dd26856ccdc98a6
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2020-07-11 16:37:13 +0700

    Switch angle when changing player direction

--- Assets/Scripts/ClockupStudio.DemonSlayer/CrosschairMovement.cs
+++ Assets/Scripts/ClockupStudio.DemonSlayer/CrosschairMovement.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace ClockupStudio.DemonSlayer
 {
-    enum Clockwises
+    internal enum ClockwiseState
     {
         Clockwise = 1,
         CounterClockwise = -1
@@ -11,54 +11,62 @@ namespace ClockupStudio.DemonSlayer
 
     struct AngleDirection
     {
-        public float Start, End;
+        public float StartAngle, EndAngle;
+        public ClockwiseState ClockwiseState;
     }
 
     public class CrosschairMovement : MonoBehaviour
     {
         public Transform PlayerPosition;
-
         public float Radius = 2f;
-
         public float Speed = 2f;
-
         public PlayerDirection PlayerDirection;
 
         private float _angle;
         private AngleDirection _angleDirection;
-
-        private Clockwises _clockwise = Clockwises.Clockwise;
+        private ClockwiseState _clockwiseState;
+        private Direction _previousPlayerDirection;
 
         private void Start()
         {
             DetectAngleFromPlayerDirection();
-            Debug.Log($"Start Angle Direction: {_angleDirection.Start}");
-            _angle = _angleDirection.Start;
+            _angle = _angleDirection.StartAngle;
+            _clockwiseState = _angleDirection.ClockwiseState;
+            _previousPlayerDirection = PlayerDirection.CurrentDirection;
         }
 
         // Update is called once per frame
         private void Update()
         {
+            if (_previousPlayerDirection != PlayerDirection.CurrentDirection)
+            {
+                DetectAngleFromPlayerDirection();
+                _angle = _angleDirection.StartAngle;
+                _clockwiseState = _angleDirection.ClockwiseState;
+                _previousPlayerDirection = PlayerDirection.CurrentDirection;
+            }
+
             var position = PlayerPosition.position;
             var x = Mathf.Cos(Mathf.PI * 2 * _angle / 360) * Radius;
             var y = Mathf.Sin(Mathf.PI * 2 * _angle / 360) * Radius;
 
             transform.position = new Vector3(x, y) + position;
-            _angle += (Time.deltaTime * Speed) * (int) _clockwise;
-            _clockwise = NextClockwise(_clockwise, _angle, _angleDirection);
+            _angle += (Time.deltaTime * Speed) * (int) _clockwiseState;
+            _clockwiseState = NextClockwise(_clockwiseState, _angle, _angleDirection);
         }
 
-        private Clockwises NextClockwise(Clockwises clockwise, float angle, AngleDirection angleDirection)
+        private ClockwiseState NextClockwise(ClockwiseState clockwiseState, float angle, AngleDirection angleDirection)
         {
-            if (clockwise == Clockwises.Clockwise && angle >= angleDirection.End)
+            if (clockwiseState == ClockwiseState.Clockwise && angle >= angleDirection.EndAngle)
             {
-                return Clockwises.CounterClockwise;
+                return ClockwiseState.CounterClockwise;
             }
-            else if (clockwise == Clockwises.CounterClockwise && angle <= angleDirection.Start)
+            else if (clockwiseState == ClockwiseState.CounterClockwise && angle <= angleDirection.StartAngle)
             {
-                return Clockwises.Clockwise;
+                return ClockwiseState.Clockwise;
             }
-            return clockwise;
+
+            return clockwiseState;
         }
 
         private void DetectAngleFromPlayerDirection()
@@ -68,15 +76,17 @@ namespace ClockupStudio.DemonSlayer
                 case Direction.Right:
                     _angleDirection = new AngleDirection
                     {
-                        Start = 0f,
-                        End = 90f
+                        StartAngle = 0f,
+                        EndAngle = 90f,
+                        ClockwiseState = ClockwiseState.Clockwise
                     };
                     break;
                 case Direction.Left:
                     _angleDirection = new AngleDirection
                     {
-                        Start = 360f,
-                        End = 270f
+                        StartAngle = 90f,
+                        EndAngle = 180f,
+                        ClockwiseState = ClockwiseState.CounterClockwise
                     };
                     break;
             }
