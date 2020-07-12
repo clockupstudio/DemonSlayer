a0a970ba3f78924bd2cb59c0f424b0d153fb7e9d
Prayoch Rujira <prayoch@clockupstudio.com>
2020-07-12 21:57:19 +0700

    Go to Game over scene when HP = 0

--- Assets/Scripts/ClockupStudio.DemonSlayer/GameManager.cs
+++ Assets/Scripts/ClockupStudio.DemonSlayer/GameManager.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using ClockupStudio.DemonSlayer;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public PlayerHealth PlayerHealth;
+    
     void Update()
     {
-        
+        Debug.Log($"Current HP ${PlayerHealth.CurrentHp}");
+        if (PlayerHealth.CurrentHp <= 0)
+        {
+            StartCoroutine(GoToEnding());
+        }
+    }
+
+    IEnumerator GoToEnding()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("EndingScene");
     }
 }
--- Assets/Scripts/ClockupStudio.DemonSlayer/PlayerHealth.cs
+++ Assets/Scripts/ClockupStudio.DemonSlayer/PlayerHealth.cs
@@ -5,7 +5,7 @@ namespace ClockupStudio.DemonSlayer
 {
     public class PlayerHealth : MonoBehaviour
     {
-        public int MaxHP = 3;
+        public int MaxHP = 1;
 
         private int _currentHP = Int32.MaxValue;
 
