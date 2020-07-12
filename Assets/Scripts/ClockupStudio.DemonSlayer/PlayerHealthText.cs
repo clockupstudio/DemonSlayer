ac5273ae74a8d3d917058e391dce3590538b3d3b
Prayoch Rujira <prayoch@clockupstudio.com>
2020-07-12 23:15:40 +0700

    Display player health

---
+++ Assets/Scripts/ClockupStudio.DemonSlayer/PlayerHealthText.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ClockupStudio.DemonSlayer
+{
+    public class PlayerHealthText : MonoBehaviour
+    {
+        public PlayerHealth PlayerHealth;
+
+        private Text _text;
+
+        private void Start()
+        {
+            _text = GetComponent<Text>();
+        }
+
+        void Update()
+        {
+            _text.text = $"LIFE: {PlayerHealth.CurrentHp}";
+        }
+    }
+}
