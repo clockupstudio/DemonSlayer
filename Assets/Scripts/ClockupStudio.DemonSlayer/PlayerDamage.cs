812571262e9239eec79fde949d033384a4c912ac
Prayoch Rujira <prayoch@clockupstudio.com>
2020-07-12 17:12:41 +0700

    Added behavior when player take damage

---
+++ Assets/Scripts/ClockupStudio.DemonSlayer/PlayerDamage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace ClockupStudio.DemonSlayer
+{
+    public class PlayerDamage : MonoBehaviour
+    {
+        private Rigidbody2D _rb2d;
+        private PlayerDirection _playerDirection;
+
+        public int BouncingForce = 500;
+
+        private void Start()
+        {
+            _rb2d = GetComponent<Rigidbody2D>();
+            _playerDirection = GetComponent<PlayerDirection>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.CompareTag("Enemies"))
+            {
+                return;
+            }
+
+            if (_playerDirection.CurrentDirection == Direction.Left)
+            {
+                _rb2d.AddForce(Vector2.right*500);
+            }
+            else
+            {
+                _rb2d.AddForce(Vector2.left*500);
+            }
+        }
+    }
+}
