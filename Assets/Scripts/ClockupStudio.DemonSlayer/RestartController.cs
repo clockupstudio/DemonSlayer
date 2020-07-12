6e4f04d89801aa0821949eaba4919fdb0a2e45e3
Prayoch Rujira <prayoch@clockupstudio.com>
2020-07-12 18:15:17 +0700

    Added ending scene

---
+++ Assets/Scripts/ClockupStudio.DemonSlayer/RestartController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ClockupStudio.DemonSlayer
+{
+    public class RestartController : MonoBehaviour
+    {
+        void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                SceneManager.LoadScene("MainScene");
+            }
+        }
+    }
+}
