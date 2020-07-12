b36e583def7516215a7a6bac7ba36d80ffd01bf6
Prayoch Rujira <prayoch@clockupstudio.com>
2020-07-12 11:39:21 +0700

    Flip enemy to correct direction

--- Assets/Scripts/ClockupStudio.DemonSlayer/EnemyFollow.cs
+++ Assets/Scripts/ClockupStudio.DemonSlayer/EnemyFollow.cs
@@ -6,21 +6,28 @@ using UnityEngine;
 public class EnemyFollow : MonoBehaviour
 {
     public Transform Target;
+    private SpriteRenderer _sprite;
 
     private bool _isAttacked = false;
+
+    private void Start()
+    {
+        _sprite = GetComponent<SpriteRenderer>();
+    }
+
     private void Update()
     {
         if (_isAttacked)
         {
             return;
         }
-        
+
+        _sprite.flipX = transform.position.x > Target.position.x;
         transform.position = Vector2.MoveTowards(transform.position, Target.position, Time.deltaTime);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
-    {
-        Debug.Log("Trigger");
+    { 
         _isAttacked = true;
     }
 
