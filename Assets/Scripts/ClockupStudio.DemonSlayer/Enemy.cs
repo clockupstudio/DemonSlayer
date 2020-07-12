7d8b61e2810399cec4e883c0d608ee0560c385c0
Thanabodee Charoenpiriyakij <thanabodee@clockupstudio.com>
2020-07-12 16:55:40 +0700

    Check moving component before processing logic

--- Assets/Scripts/ClockupStudio.DemonSlayer/Enemy.cs
+++ Assets/Scripts/ClockupStudio.DemonSlayer/Enemy.cs
@@ -1,26 +1,36 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 namespace ClockupStudio.DemonSlayer
 {
     public class Enemy : MonoBehaviour
     {
-        public int Health = 1;
+        public uint Health = 1;
+
+        private void Update()
+        {
+            if (Health == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
             var moveState = other.GetComponent<PlayerMovementReadOnlyState>();
-            if (moveState != null && moveState.MovingState != PlayerMovingState.Moving)
+            if (moveState == null)
+            {
+                Debug.Log("Player doesn't have movement read-only state. Health doesn't decrease.");
+                return;
+            }
+            if (moveState.MovingState != PlayerMovingState.Moving)
             {
                 Debug.Log("Player doesn't attack. Health doesn't decrease.");
                 return;
             }
 
             Health--;
-            if (Health == 0)
-            {
-                Destroy(gameObject);
-            }
         }
     }
 }
