cf5998d3400a5427670d6e693a85aa51533d5046
Prayoch Rujira <prayoch@clockupstudio.com>
2020-07-12 11:29:58 +0700

    Added EnemyFollow

---
+++ Assets/Scripts/ClockupStudio.DemonSlayer/EnemyFollow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public Transform Target;
+
+    private bool _isAttacked = false;
+    private void Update()
+    {
+        if (_isAttacked)
+        {
+            return;
+        }
+        
+        transform.position = Vector2.MoveTowards(transform.position, Target.position, Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Trigger");
+        _isAttacked = true;
+    }
+
+}
